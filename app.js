7d5d382387bbf5e2b86c3eb6cea296ed90ad10ab
Ryan Fernandez <ryaaanf3@gmail.com>
2021-05-24 14:01:40 -0700

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
+const passport = require('passport');
+const authenticate = require('./authenticate');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
@@ -46,25 +49,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+//only necessary if using session based authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
+  if(!req.user) {
     const err = new Error('You are not authenticated!');
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === 'authenticated') {
       return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
   }
 }
 
