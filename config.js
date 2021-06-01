d3e3b161672e54bd80df40b1ef284c6ced7d3e4a
Ryan Fernandez <ryaaanf3@gmail.com>
2021-06-01 15:45:01 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '484931882818246',
+        clientSecret: '916f731efb40023fbee3a45b63694a8b'
+    }
 }
